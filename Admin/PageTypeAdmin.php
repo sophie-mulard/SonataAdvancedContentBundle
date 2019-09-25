1f5f7d66984d071f325abcba6e6f582f559c4fa1
sophie-mulard <smulard@gmail.com>
2019-09-25 12:30:44 +0200

    Update Standalone version to last features

--- Admin/PageTypeAdmin.php
+++ Admin/PageTypeAdmin.php
@@ -2,18 +2,39 @@
 
 namespace Sherlockode\SonataAdvancedContentBundle\Admin;
 
+use Sherlockode\AdvancedContentBundle\Form\Type\PageTypeType;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class PageTypeAdmin extends AbstractAdmin
 {
     public function configureFormFields(FormMapper $form)
     {
         $form
-            ->add('name', TextType::class, ['label' => 'page_type.form.name'])
+            ->add('name')
         ;
+    }
+
+    private function getPageTypeFormBuilder()
+    {
+        return $this->getFormContractor()->getFormFactory()
+            ->createNamedBuilder($this->getUniqid(), PageTypeType::class);
+    }
+
+    public function getFormBuilder()
+    {
+        $formBuilder = $this->getPageTypeFormBuilder();
+        $this->defineFormBuilder($formBuilder);
+
+        return $formBuilder;
+    }
+
+    public function defineFormBuilder(FormBuilderInterface $formBuilder)
+    {
+        $formBuilder = $this->getPageTypeFormBuilder();
+        parent::defineFormBuilder($formBuilder);
     }
 
     public function configureListFields(ListMapper $list)
